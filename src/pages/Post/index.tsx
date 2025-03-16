0fec24670dd46315fb52deb89a8b297d52dcb14a
Igor Solly <igor@guemp.com>
2025-03-16 17:03:25 -0300

    Criação do componente PostContent

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,11 @@
 import { PostInfoCard } from "../../components/PostInfoCard/index";
+import { PostContent } from "../../components/PostContent/index";
 
 export function Post() {
   return (
     <>
       <PostInfoCard />
+      <PostContent />
     </>
   );
 }
