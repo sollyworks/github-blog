9f6140f84ca43ff9e86707c746b02263b6581c50
Igor Solly <igor@guemp.com>
2025-03-16 12:50:08 -0300

    Consumo de API de Users do GitHub

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -1,10 +1,10 @@
+import React from "react";
 import {
   ProfileCardContainer,
   ProfileCardImage,
   ProfileInfoHead,
   ProfileListLinks,
 } from "./styles";
-import avatar from "../../assets/avatar.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBuilding,
@@ -12,39 +12,59 @@ import {
   faArrowUpRightFromSquare,
 } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { api } from "../../libs/axios";
+
+interface User {
+  html_url: string;
+  login: string;
+  name: string;
+  avatar_url: string;
+  bio: string;
+  company: string | null;
+  followers: number;
+}
 
 export function ProfileCard() {
+  const [user, setUser] = React.useState<User>({} as User);
+
+  React.useEffect(() => {
+    async function getUser() {
+      const response = await api.get("/users/sollyworks");
+      const user = await response.data;
+
+      setUser(user);
+    }
+
+    getUser();
+  }, []);
+
   return (
     <section style={{ padding: "0rem 1rem" }}>
       <ProfileCardContainer>
         <ProfileCardImage>
-          <img src={avatar} alt="User Profile Picture" />
+          <img src={user.avatar_url} alt="User Profile Picture" />
         </ProfileCardImage>
         <div>
           <ProfileInfoHead>
-            <h2>Cameron Williamson</h2>
-            <a href="#" target="_blank" rel="noopener noreferrer">
+            <h2>{user.name}</h2>
+            <a href={user.html_url} target="_blank" rel="noopener noreferrer">
               Github
               <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
             </a>
           </ProfileInfoHead>
-          <p>
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-            volutpat pulvinar vel mass.
-          </p>
+          <p>{user.bio}</p>
           <ProfileListLinks>
             <li>
               <FontAwesomeIcon icon={faGithub} />
-              <a href="#">cameronwll</a>
+              <span>{user.login}</span>
             </li>
             <li>
               <FontAwesomeIcon icon={faBuilding} />
-              <a href="#">Rocketseat</a>
+              <span>{user.company ? user.company : "Nenhuma"}</span>
             </li>
             <li>
               <FontAwesomeIcon icon={faUserGroup} />
-              <a href="#">32 seguidores</a>
+              <span>{user.followers} seguidores</span>
             </li>
           </ProfileListLinks>
         </div>
